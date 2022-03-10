e1f0bdabd28d055eeb7e45f0dc2c0f627c875a3f
JSWalker317 <josvanson317@gmail.com>
2022-03-10 16:47:56 +0700

    Merge remote-tracking branch 'github1/master'
    # Conflicts:
    #	app/src/main/res/drawable/background_design.xml
    #	app/src/main/res/drawable/img_welcome.png
    #	app/src/main/res/layout/activity_welcome.xml

---
+++ app/src/main/java/com/example/fooddelivery/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.example.fooddelivery
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+private const val SPLASH = 4000
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+//        val intent = Intent(this,WelcomeActivity::class.java)
+//        startActivity(intent)
+
+        var topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        var bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+        var imageView = findViewById<ImageView>(R.id.img_intro)
+        var textView = findViewById<TextView>(R.id.tv_intro)
+
+        imageView.setAnimation(topAnim)
+        textView.setAnimation(bottomAnim)
+
+        Handler().postDelayed({
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH.toLong())
+    }
+}
